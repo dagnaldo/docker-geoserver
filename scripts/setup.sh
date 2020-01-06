879d6e62829e52ff2e9c9fe2201056e1c4f6445a
dagnaldo <naldo.penha@gmail.com>
2020-01-06 17:25:01 -0300

    geopackage plugin

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -40,7 +40,7 @@ array=(geoserver-$GS_VERSION-vectortiles-plugin.zip geoserver-$GS_VERSION-css-pl
 geoserver-$GS_VERSION-csw-plugin.zip geoserver-$GS_VERSION-wps-plugin.zip geoserver-$GS_VERSION-printing-plugin.zip \
 geoserver-$GS_VERSION-libjpeg-turbo-plugin.zip geoserver-$GS_VERSION-control-flow-plugin.zip \
 geoserver-$GS_VERSION-pyramid-plugin.zip geoserver-$GS_VERSION-gdal-plugin.zip \
-geoserver-$GS_VERSION-sldservice-plugin.zip  \
+geoserver-$GS_VERSION-sldservice-plugin.zip  geoserver-$GS_VERSION-geopkg-plugin.zip \
 geoserver-$GS_VERSION-importer-plugin.zip geoserver-$GS_VERSION-charts-plugin.zip)
 for i in "${array[@]}"
 do
